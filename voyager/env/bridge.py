7409e76afb81f4995da31ef4804c5b6183527e74
Hiro <huc.mobile@gmail.com>
2025-06-09 12:49:33 +0900

    Fix retry logic when restarting mineflayer

--- voyager/env/bridge.py
+++ voyager/env/bridge.py
@@ -85,6 +85,7 @@ class VoyagerEnv(gym.Env):
             print("Mineflayer process has exited, restarting")
             self.mineflayer.run()
             if not self.mineflayer.is_running:
+                retry += 1
                 if retry > 3:
                     raise RuntimeError("Mineflayer process failed to start")
                 else:
